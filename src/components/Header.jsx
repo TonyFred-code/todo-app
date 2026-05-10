54d67afb8540c5a6b0db5c58d4df5b3fc1e07939
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-05-10 19:22:43 +0100

    feat: create `Header` comp to store page header

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header>
+      <h1 className="uppercase font-bold tracking-[0.3em] text-white text-3xl">
+        todo
+      </h1>
+    </header>
+  );
+}
