5ffedd7e6a1fad761650ad3d8037fc4f7367f046
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-05-11 10:25:25 +0100

    feat: create theme context and its provider
    create a theme context to prevent prop drilling

---
+++ src/context/ThemeProvider.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { getDefaultTheme } from "../lib/themeUtils.js";
+import ThemeContext from "./ThemeContext.jsx";
+import { element } from "prop-types";
+import { THEME_STORAGE_KEY, THEMES } from "../constants/theme.js";
+
+export default function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState(getDefaultTheme);
+
+  useEffect(() => {
+    const root = document.documentElement;
+
+    root.classList.toggle(THEMES.DARK, theme === THEMES.DARK);
+    localStorage.setItem(THEME_STORAGE_KEY, theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+ThemeProvider.propTypes = {
+  children: element.isRequired,
+};
