33c89bd012690a1d0c227bf040f453dfd481e404
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-05-15 12:33:15 +0100

    docs: update theme key comment to match var name

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,6 +1,6 @@
 const THEME_STORAGE_KEY = "todo_app_theme_local_storage_key";
 /* 
-NOTE: keep theme_storage_key in sync with flash-screen prevention script
+NOTE: keep THEME_STORAGE_KEY in sync with flash-screen prevention script
 in index.html script tag
 */
 
